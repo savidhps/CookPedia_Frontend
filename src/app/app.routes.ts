c08d6b457b5c07542c2dc34310534940ccf62fe6
savidhps <savidhps00@gmail.com>
2025-05-31 17:01:50 +0530

    adding important library,Created components login,about,recipe,Home,login,register,viewRecipe,pagenotFound,contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ViewRecipeComponent } from './view-recipe/view-recipe.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'register',component:RegisterComponent},
+    {path:'login',component:LoginComponent},
+    {path:'all-recipes',component:RecipeComponent},
+    {path:'about',component:AboutComponent},
+    {path:'contact',component:ContactComponent},
+    {path:'view/:id',component:ViewRecipeComponent},
+    {path:'**',component:PagenotfoundComponent}
+
+];
---
+++ src/app/view-recipe/view-recipe.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-recipe',
+  imports: [],
+  templateUrl: './view-recipe.component.html',
+  styleUrl: './view-recipe.component.css'
+})
+export class ViewRecipeComponent {
+
+}
